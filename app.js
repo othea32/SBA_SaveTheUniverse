273fa6618d6c3eab9728a82e354b678150a4cd62
othea32 <othea32@gmail.com>
2023-06-23 12:02:28 -0700

    updated app.js and styles.css

--- app.js
+++ app.js
@@ -39,130 +39,130 @@
 
 // Alien class
 class Alien {
- constructor() {
+  constructor() {
    this.hull = Math.floor(Math.random() * 4) + 3;
    this.firepower = Math.floor(Math.random() * 3) + 2;
    this.accuracy = (Math.floor(Math.random() * 3) + 6) / 10;
- }
+  }
 }
 
 // Game object
 const game = {
- aliens: [],
- alienIndex: 0,
- player: {
-   hull: 20,
-   firepower: 5,
-   accuracy: 0.7
- },
- gameLog: document.getElementById("gameLog"),
- startGameBtn: document.getElementById("startGame"),
- attackGameBtn: document.getElementById("attackGame"),
- retreatGameBtn: document.getElementById("retreatGame"),
+  aliens: [],
+  alienIndex: 0,
+  player: {
+    hull: 20,
+    firepower: 5,
+    accuracy: 0.7
+  },
+  gameLog: document.getElementById("gameLog"),
+  startGameBtn: document.getElementById("startGame"),
+  attackGameBtn: document.getElementById("attackGame"),
+  retreatGameBtn: document.getElementById("retreatGame"),
 
  // Initialize the game
- init() {
-   this.startGameBtn.addEventListener("click", () => this.startGame());
-   this.attackGameBtn.addEventListener("click", () => this.attackGame());
-   this.retreatGameBtn.addEventListener("click", () => this.retreatGame());
- },
+  init() {
+    this.startGameBtn.addEventListener("click", () => this.startGame());
+    this.attackGameBtn.addEventListener("click", () => this.attackGame());
+    this.retreatGameBtn.addEventListener("click", () => this.retreatGame());
+  },
 
- 
+
  // Start the game
- startGame() {
-  this.startGameBtn.disabled = true;
-  this.createAliens();
-  this.showGameLog("Game started! You are the captain of the USS Assembly.", "color: green; font-weight: bold;");
-  this.showGameLog("You have encountered a horde of aliens. Your mission is to destroy them all!", "color: green; font-weight: bold;");
-  this.showGameLog(`You are currently battling Alien Ships ${this.alienIndex + 1}.`, "color: green; font-weight: bold;");
-  this.showGameLog(`Alien Ship ${this.alienIndex + 1} has hull: ${this.aliens[this.alienIndex].hull}`, "color: green; font-weight: bold;");
-  this.showGameLog(`You have hull: ${this.player.hull}`, "color: green; font-weight: bold;");
-  this.enableButtons();
+  startGame() {
+    this.startGameBtn.disabled = true;
+    this.createAliens();
+    this.showGameLog("Game started! You are the captain of the USS Assembly.", "color: green; font-weight: bold;");
+    this.showGameLog("You have encountered a horde of aliens. Your mission is to destroy them all!", "color: green; font-weight: bold;");
+    this.showGameLog(`You are currently battling Alien Ships ${this.alienIndex + 1}.`, "color: green; font-weight: bold;");
+    this.showGameLog(`Alien Ship ${this.alienIndex + 1} has hull: ${this.aliens[this.alienIndex].hull}`, "color: green; font-weight: bold;");
+    this.showGameLog(`You have hull: ${this.player.hull}`, "color: green; font-weight: bold;");
+    this.enableButtons();
 },
 
 // Create alien ships
 createAliens() {
- for (let i = 0; i < 6; i++) {
-   const alien = new Alien();
-   this.aliens.push(alien);
- }
+  for (let i = 0; i < 6; i++) {
+    const alien = new Alien();
+    this.aliens.push(alien);
+  }
 },
 
 // Attack
 attackGame() {
- const alien = this.aliens[this.alienIndex];
- const playerAccuracy = Math.random();
+  const alien = this.aliens[this.alienIndex];
+  const playerAccuracy = Math.random();
 
- if (playerAccuracy <= this.player.accuracy) {
-   alien.hull -= this.player.firepower;
-   this.showGameLog(`You attacked Alien Ship ${this.alienIndex + 1} and dealt ${this.player.firepower} damage.`, "color: blue; font-weight: bold;");
- } else {
-   this.showGameLog("Your attack missed!", "color: red; font-weight: bold;");
- }
+  if (playerAccuracy <= this.player.accuracy) {
+    alien.hull -= this.player.firepower;
+    this.showGameLog(`You attacked Alien Ship ${this.alienIndex + 1} and dealt ${this.player.firepower} damage.`, "color: blue; font-weight: bold;");
+  } else {
+    this.showGameLog("Your attack missed!", "color: red; font-weight: bold; font-size: 20px;");
+  }
 
- if (alien.hull > 0) {
-   this.alienAttack(alien);
- } else {
-   this.alienIndex++;
-   if (this.alienIndex < this.aliens.length) {
-     this.showGameLog(`You destroyed Alien Ship ${this.alienIndex}.`, "color: green; font-weight: bold;");
-     this.showGameLog(`You are now battling Alien Ship ${this.alienIndex + 1}.`, "color: blue; font-weight: bold;");
-     this.showGameLog(`Alien Ship ${this.alienIndex + 1} has hull: ${this.aliens[this.alienIndex].hull}`, "color: blue; font-weight: bold;");
-     this.showGameLog(`You have hull: ${this.player.hull}`, "color: blue; font-weight: bold;");
-   } else {
-     this.endGame(true);
-   }
- }
+  if (alien.hull > 0) {
+    this.alienAttack(alien);
+  } else {
+    this.alienIndex++;
+    if (this.alienIndex < this.aliens.length) {
+      this.showGameLog(`You destroyed Alien Ship ${this.alienIndex}.`, "color: green; font-weight: bold; font-size: 20px;");
+      this.showGameLog(`You are now battling Alien Ship ${this.alienIndex + 1}.`, "color: blue; font-weight: bold;");
+      this.showGameLog(`Alien Ship ${this.alienIndex + 1} has hull: ${this.aliens[this.alienIndex].hull}`, "color: blue; font-weight: bold;");
+      this.showGameLog(`You have hull: ${this.player.hull}`, "color: blue; font-weight: bold;");
+    } else {
+      this.endGame(true);
+    }
+  }
 },
 
 // Alien's attack
 alienAttack(alien) {
- const alienAccuracy = Math.random();
+  const alienAccuracy = Math.random();
 
- if (alienAccuracy <= alien.accuracy) {
-   this.player.hull -= alien.firepower;
-   this.showGameLog(`Alien Ship ${this.alienIndex + 1} attacked you and dealt ${alien.firepower} damage.`, "color: red; font-weight: bold;");
- } else {
-   this.showGameLog(`Alien Ship ${this.alienIndex + 1}'s attack missed!`, "color: green; font-weight: bold;");
- }
+  if (alienAccuracy <= alien.accuracy) {
+    this.player.hull -= alien.firepower;
+    this.showGameLog(`Alien Ship ${this.alienIndex + 1} attacked you and dealt ${alien.firepower} damage.`, "color: red; font-weight: bold; font-size: 20px;");
+  } else {
+    this.showGameLog(`Alien Ship ${this.alienIndex + 1}'s attack missed!`, "color: green; font-weight: bold; font-size: 20px;");
+  }
 
- if (this.player.hull > 0) {
-   this.showGameLog(`You have hull: ${this.player.hull}`, "color: blue; font-weight: bold;");
- } else {
-   this.endGame(false);
- }
+  if (this.player.hull > 0) {
+    this.showGameLog(`You have hull: ${this.player.hull}`, "color: blue; font-weight: bold;");
+  } else {
+    this.endGame(false);
+  }
 },
 
 // Retreat
 retreatGame() {
- this.endGame(false);
+  this.endGame(false);
 },
 
 // End the game
 endGame(isVictory) {
- if (isVictory) {
-   this.showGameLog("Congratulations! You have destroyed all the alien ships. Earth is saved!", "color: green; font-weight: bold;");
- } else {
-   this.showGameLog("Game over! The aliens have destroyed your ship. Earth is doomed...", "color: red; font-weight: bold;");
- }
- this.disableButtons();
+  if (isVictory) {
+    this.showGameLog("Congratulations! You have destroyed all the alien ships. Earth is saved!", "color: green; font-weight: bold; font-size: 20px;");
+  } else {
+    this.showGameLog("Game over! The aliens have destroyed your ship. Earth is doomed...", "color: red; font-weight: bold; font-size: 20px;");
+  }
+  this.disableButtons();
 },
 
 // Show game log messages
 showGameLog(message, style) {
- // this.gameLog.innerText += `${message}`;
- this.gameLog.innerHTML += `<p style="${style}">${message}</p>`;
+ // this.gameLog.innerText += `${message},${style}`;
+  this.gameLog.innerHTML += `<p style="${style}">${message}</p>`;
 },
 
 // Game buttons
 enableButtons() {
- this.attackGameBtn.disabled = false;
- this.retreatGameBtn.disabled = false;
+  this.attackGameBtn.disabled = false;
+  this.retreatGameBtn.disabled = false;
 },
 
 disableButtons() {
- this.attackGameBtn.disabled = true;
- this.retreatGameBtn.disabled = true;
+  this.attackGameBtn.disabled = true;
+  this.retreatGameBtn.disabled = true;
 }
 };
 
